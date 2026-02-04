b6ac5fc600e9216797781e2031f55c2b7ad7a264
Raimundo Herrera <rjherrera@uc.cl>
2026-02-04 17:17:11 -0300

    feat(0xparc): improve logging and debugging

--- 0xPARC/puzzle.py
+++ 0xPARC/puzzle.py
@@ -1,4 +1,5 @@
 from math import ceil, log10
+import logging
 
 def dot_product(a, b):
     if len(a) != len(b):
@@ -14,17 +15,23 @@ class Game:
 
     def oracle(self, x):
         self.n_operations += 1
+        logging.debug(f'Oracle call number: {self.n_operations}')
         return dot_product(self.secret_list, x)
 
     def guess_list(self):
         x_1 = [1 for i in range(self.n)]
+        logging.debug(f'List for operation *sum*: {x_1}')
         question_1 = self.oracle(x_1)
+        logging.debug(f'Oracle answer for operation *sum*: {question_1}')
 
         grade = ceil(log10(question_1))
         x_2 = [10 ** (grade * i) for i in range(self.n)]
+        logging.debug(f'List for operation *separate*: {x_2}')
         question_2 = self.oracle(x_2)
+        logging.debug(f'Oracle answer for operation *separate*: {question_2}')
 
         v_candidate = [question_2 % x_2[i + 1] // x_2[i] for i in range(len(x_2) - 1)] + [question_2 // x_2[-1]]
+        logging.debug(f'Guessed list: {v_candidate}')
         return v_candidate
 
     def experiment(self):
@@ -32,11 +39,13 @@ class Game:
 
 
 if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO)
+
     game = Game([2, 200, 400, 10, 20, 14])
 
     if game.experiment():
-        print('Result matches target.')
-        print(f'Discovered secret list {game.secret_list} using {game.n_operations} oracle operations.')
+        logging.info('Result matches target.')
+        logging.info(f'Discovered secret list {game.secret_list} using {game.n_operations} oracle operations.')
     else:
-        print('No match.')
-        print(f'Secret list was {game.secret_list}.')
+        logging.info('No match.')
+        logging.info(f'Secret list was {game.secret_list}.')
